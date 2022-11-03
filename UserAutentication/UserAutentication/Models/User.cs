3db73871767ec94cf9b6291b2faa0a5f1ae8e0ad
Ubaydulayev <ubaydullayevdastonbek@gmail.com>
2022-11-03 17:57:46 +0500

    migrations

--- UserAutentication/UserAutentication/Models/User.cs
+++ UserAutentication/UserAutentication/Models/User.cs
@@ -5,6 +5,7 @@ namespace UserAutentication.Models;
 //[Keyless]
 public class User
 {
+    public int Id { get; set; }
     public string? Username { get; set; }
     public string? Password { get; set; }
 }
